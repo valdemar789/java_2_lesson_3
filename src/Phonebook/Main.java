4354c4427c3adb3a0385302d386390519769a76a
valdemar789 <kuznetsov.ivan789@gmail.com>
2020-10-29 11:39:54 +0300

    10/29

--- src/Phonebook/Main.java
+++ src/Phonebook/Main.java
@@ -1,4 +1,22 @@
 package Phonebook;
 
 public class Main {
+
+    public static void main(String[] args){
+
+        Phonebook phonebook = new Phonebook();
+
+        phonebook.add("Лисичкина", "89161111111");
+        phonebook.add("Лисичкина", "89161111112");
+        phonebook.add("Лисичкина", "89161111113");
+        phonebook.add("Воробьев", "89161111114");
+        phonebook.add("Волков", "89161111115");
+
+
+        System.out.println("Лисичкина");
+        System.out.println(phonebook.get("Лисичкина"));
+        System.out.println("Воробьев");
+        System.out.println(phonebook.get("Воробьев"));
+
+    }
 }
--- src/Words/Main.java
+++ src/Words/Main.java
@@ -1,4 +1,25 @@
 package Words;
 
-public class Main {
-}
+import java.util.*;
+
+    public class Main {
+        public static void main(String[] args){
+            List<String> words = Arrays.asList(
+                    "Береза", "Осина", "Дуб", "Осина", "Ель",
+                    "Дуб", "Дуб", "Клен", "Осина",
+                    "Сосна", "Дуб", "Береза"
+            );
+
+            Set<String> unique = new HashSet<String>(words);
+
+            System.out.println("Начальный массив");
+            System.out.println(words.toString());
+            System.out.println("Уникальные слова");
+            System.out.println(unique.toString());
+            System.out.println("Сколько раз встречается каждое слово");
+            for (String key : unique) {
+                System.out.println(key + ": " + Collections.frequency(words, key));
+            }
+        }
+    }
+
